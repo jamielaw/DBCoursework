32f60e9b1d1f35678a180cbc61cb404afd6517f6
Diana Darie <zcabdad@ucl.ac.uk>
2017-01-18 17:00:07 +0000

    fetching data - reading/updating users table

--- sn/profile/index.php
+++ sn/profile/index.php
@@ -1,13 +1,54 @@
-<?php
-$title = "Profile";
-$description = "";
-include("inc/header.php");
- ?>
-  <body>
-    <!--  Navigation-->
-    <?php include 'inc/nav-trn.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <script src="../js/min/bootstrap.min.js"></script>
+</head>
 
-    <img src="http://placehold.it/100/100" alt="Profile Picture">
-    <h1>$FIRSTNAME $SECONDNAME</h1>
-
-    <?php include 'inc/footer.php'; ?>
+<body>
+    <div class="container">
+    		<div class="row">
+    			<h3>PHP CRUD Grid</h3>
+    		</div>
+			<div class="row">
+				<p>
+					<a href="create.php" class="btn btn-success">Create</a>
+				</p>
+				
+				<table class="table table-striped table-bordered">
+		              <thead>
+		                <tr>
+		                  <th>Email</th>
+		                  <th>First Name</th>
+		                  <th>Last Name</th>
+		                  <th>Action</th>
+		                </tr>
+		              </thead>
+		              <tbody>
+		              <?php 
+					   include '..\database.php';
+					   $pdo = Database::connect();
+					   $sql = 'SELECT * FROM users ORDER BY email';
+	 				   foreach ($pdo->query($sql) as $row) {
+						   		echo '<tr>';
+							   	echo '<td>'. $row['email'] . '</td>';
+							   	echo '<td>'. $row['firstName'] . '</td>';
+							   	echo '<td>'. $row['lastName'] . '</td>';
+							   	echo '<td width=250>';
+							   	echo '<a class="btn" href="read.php?email='.$row['email'].'">Read</a>';
+							   	echo '&nbsp;';
+							   	echo '<a class="btn btn-success" href="update.php?email='.$row['email'].'">Update</a>';
+							   	echo '&nbsp;';
+							   	echo '<a class="btn btn-danger" href="delete.php?email='.$row['email'].'">Delete</a>';
+							   	echo '</td>';
+							   	echo '</tr>';
+					   }
+					   Database::disconnect();
+					  ?>
+				      </tbody>
+	            </table>
+    	</div>
+    </div> <!-- /container -->
+  </body>
+</html>

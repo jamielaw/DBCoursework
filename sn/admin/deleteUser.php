93e7dac9b34aeb0aa82042946de09c4ee85f7fe7
vickyd885 <vickyd885@gmail.com>
2017-01-20 17:29:39 +0000

    change index page

--- sn/admin/deleteUser.php
+++ sn/admin/deleteUser.php
@@ -1,32 +1,37 @@
 <?php
 
-
-function redirect($url) {
-  ob_start();
-  header('Location: '.$url);
-  ob_end_flush();
-  die();
-}
+  require '../databases.php';
 
 
-  $argument1 = $_GET['email'];
-  //echo $argument1;
-
-  echo $argument1;
-  require '../database.php';
-
-  // sql to delete a record
-  $sql = "DELETE FROM MyDB.users WHERE email=". "'" . $argument1 . "'";
-  //echo $sql;
-
-  if ($conn->query($sql) === TRUE) {
-      //echo "Record deleted successfully";
-  } else {
-      //echo "Error deleting record: " . $conn->error;
+  //function to redirect
+  function redirect($url) {
+    ob_start();
+    header('Location: '.$url);
+    ob_end_flush();
+    die();
   }
 
-  $conn->close();
+  function wrapArgument($arg){
+    return "'" . $arg . "'";
+  }
 
+  function removeSpaces($arg){
+    return str_replace(' ', '', $arg);
+  }
+
+
+  $argument1 = removeSpaces(wrapArgument($_GET['email']));
+
+  // sql to delete a record
+  // $sql = "DELETE FROM MyDB.users WHERE email=". "'" . $argument1 . "'";
+  $sql = "DELETE FROM users WHERE email=?";
+
+  // come back and fix logic for this + handle error case
+  $pdo = Database::connect();
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $q = $pdo->prepare($sql);
+  $q->execute($argument1);
+  Database::disconnect();
 
 
   //echo "here";
--- sn/database.php
+++ sn/database.php
@@ -1,34 +1,34 @@
 <?php
-class Database 
+class Database
 {
-	private static $dbName = 'mydb' ; 
-	private static $dbHost = 'localhost' ;
+	private static $dbName = 'mydb' ;
+	private static $dbHost = 'localhost';
 	private static $dbUsername = 'root';
-	private static $dbUserPassword = 'admin';
-	
+	private static $dbUserPassword = 'root';
+
 	private static $cont  = null;
-	
+
 	public function __construct() {
 		exit('Init function is not allowed');
 	}
-	
+
 	public static function connect()
 	{
 	   // One connection through whole application
        if ( null == self::$cont )
-       {      
-        try 
+       {
+        try
         {
-          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);  
+          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);
         }
-        catch(PDOException $e) 
+        catch(PDOException $e)
         {
-          die($e->getMessage());  
+          die($e->getMessage());
         }
-       } 
+       }
        return self::$cont;
 	}
-	
+
 	public static function disconnect()
 	{
 		self::$cont = null;

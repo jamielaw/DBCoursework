27b98a5cd14aa106b1e3aaf534782a9e8fa8e1a9
vickyd885 <vickyd885@gmail.com>
2017-01-20 17:54:06 +0000

    change deleting stuff

--- sn/admin/deleteUser.php
+++ sn/admin/deleteUser.php
@@ -1,7 +1,6 @@
 <?php
 
-  require '../databases.php';
-
+  require '../database.php';
 
   //function to redirect
   function redirect($url) {
@@ -11,29 +10,18 @@
     die();
   }
 
-  function wrapArgument($arg){
-    return "'" . $arg . "'";
-  }
-
-  function removeSpaces($arg){
-    return str_replace(' ', '', $arg);
-  }
-
-
-  $argument1 = removeSpaces(wrapArgument($_GET['email']));
-
+  //$argument1 = removeSpaces(wrapArgument($_GET['email']));
+  $argument1 = $_GET['email'];
   // sql to delete a record
-  // $sql = "DELETE FROM MyDB.users WHERE email=". "'" . $argument1 . "'";
-  $sql = "DELETE FROM users WHERE email=?";
-
+  $sql = "DELETE FROM users WHERE email=". "'" . $argument1 . "'";
+  //$sql = "DELETE FROM users WHERE email=". $argument1;
+  #echo $sql;
+  //$sql = "DELETE FROM users WHERE email=?";
   // come back and fix logic for this + handle error case
   $pdo = Database::connect();
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $q = $pdo->prepare($sql);
-  $q->execute($argument1);
+  $pdo->exec($sql);
   Database::disconnect();
-
-
   //echo "here";
 
   redirect('http://localhost:8888/sn/admin/');

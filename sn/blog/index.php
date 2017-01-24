c2f8ad4b7148eb56cb1528e55b4917d7c1858b3b
vickyd885 <vickyd885@gmail.com>
2017-01-24 16:01:36 +0000

    initial ui setup

--- sn/blog/index.php
+++ sn/blog/index.php
@@ -3,11 +3,53 @@
 $title = "Blog";
 $description = "";
 include("../inc/header.php");
- ?>
-  <body>
-    <!--  Navigation-->
-    <?php include '../inc/nav-trn.php'; ?>
+?>
+<body>
+  <?php include '../inc/nav-trn.php'; ?>
+  <div class="container">
+    <div class="blog-container">
+      <div class="row">
+        <font size="10"> Blogs </font> <br>
+        <font size="3"> You can write blogposts or view your friends. </font>
+      </div>
 
-    <h1>Blog</h1>
-    <!-- Footer  -->
-    <?php include '../inc/footer.php'; ?>
+      <div class="row">
+        <div class="blog-section">
+          <input class="blog-search-bar" type="text" name="search" placeholder="Search posts">
+        </div>
+
+        <p>
+          Posts you've written:
+        </p>
+        <div class="blog-section">
+          <div class="blog-section personal-post-container">
+            Post Title
+          </div>
+
+        </div>
+
+        <p>
+          Posts your friends have written:
+        </p>
+
+        <div class="blog-section">
+          <div class="blog-section friend-post-container">
+            Post Title 
+          </dvi>
+        </div>
+      </div>
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+  </div>
+
+</body>

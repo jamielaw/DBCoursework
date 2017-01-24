abe5a8c9c6e0264abc33287844b36ebbe049334d
JAMIE LAW <jamie.law.14@ucl.ac.uk>
2017-01-24 22:12:34 +0000

    Search now has typeahead suggestions (max 5)

--- sn/inc/nav-trn.php
+++ sn/inc/nav-trn.php
@@ -3,9 +3,55 @@
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="http://code.jquery.com/jquery-3.0.0.min.js"></script>
+  <script type="text/javascript" src="//netsh.pp.ua/upwork-demo/1/js/typeahead.js"></script>
+  <script type ="text/javascript">
+    jQuery(document).ready(function($) {
+      $('input.submit').typeahead({
+        name: 'submit',
+        remote: {url: '/sn/inc/nav-trnsearch.php?query=%QUERY'} //absolute ref as we don't know which page is calling this
+      });
+    });
+    // jQuery('#input').on('input', function(){ //backup plan
+    //   $searchQuery = $("#input").val();
+    //   window.alert($searchQuery);
+    // });
+  </script>
+  <style>
+
+        .content {
+            width: 80%;
+            margin: 0 auto;
+            margin-top: 50px;
+        }
+
+        .tt-hint,
+        .submit {
+            border: 2px solid #CCCCCC;
+            border-radius: 8px 8px 8px 8px;
+            font-size: 24px;
+            height: 45px;
+            line-height: 30px;
+            outline: medium none;
+            padding: 8px 12px;
+            width: 400px;
+        }
+
+        .tt-dropdown-menu {
+            width: 400px;
+            margin-top: 5px;
+            padding: 8px 12px;
+            background-color: #fff;
+            border: 1px solid #ccc;
+            border: 1px solid rgba(0, 0, 0, 0.2);
+            border-radius: 8px 8px 8px 8px;
+            font-size: 18px;
+            color: #111;
+            background-color: #F1F1F1;
+        }
+</style>
 </head>
+
 
 <nav class="navbar navbar-inverse navbar-fixed-top">
   <div class="container-fluid">
@@ -33,8 +79,8 @@
 
       <!--search bar-->
       <form class="navbar-form navbar-left" action="../search/searchresult.php?go" method="get" id="searchform">
-          <input type="text" class="form-control" placeholder="Search for friends" name="submit" id="input">
-        <button type="submit" id="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
+          <input type="text" class="submit" placeholder="Search for friends" name="submit" id="submit" autocomplete="off">
+        <button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
       </form>
 
     </div>
@@ -42,12 +88,6 @@
 </nav>
 </html>
 
-<script>
-// jQuery('#input').on('input', function(){ //backup plan
-//   $searchQuery = $("#input").val();
-//   window.alert($searchQuery);
-// });
-</script>
 <style type="text/css">
   body { padding-top: 50px; }
 </style>

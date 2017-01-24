63c2c18f48e5636347c329b67149cd36e5263e12
JAMIE LAW <jamie.law.14@ucl.ac.uk>
2017-01-24 22:22:32 +0000

    Allow search results to have unfinished surnames in full name search
    e.g. “Charles Babb” now will return “Charles Babbage”

--- sn/search/searchresult.php
+++ sn/search/searchresult.php
@@ -22,7 +22,7 @@
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             //-query  the database table 
             //currently searches to see if the first or last name contains the search string at any location
-            $sql="SELECT email, firstName, lastName, profileImage FROM MyDB.users WHERE firstName LIKE '" . $name .  "%' OR lastName LIKE '" . $name ."%' OR concat_ws(' ', firstName, lastName) LIKE '" . $name . "'"; 
+            $sql="SELECT email, firstName, lastName, profileImage FROM MyDB.users WHERE firstName LIKE '" . $name .  "%' OR lastName LIKE '" . $name ."%' OR concat_ws(' ', firstName, lastName) LIKE '" . $name . "%'"; 
             //echo $sql;
 
             echo "<table style='width:100%'> <tr> <th> Email </th> <th> First name </th> <th> Last Name </th> <th> Image </th>";

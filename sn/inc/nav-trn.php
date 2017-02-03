0488797b9adafc32792f1a22ef0b8bbcd97518bc
Diana Darie <zcabdad@ucl.ac.uk>
2017-02-03 08:27:22 +0000

    changing paths in navbar

--- sn/inc/nav-trn.php
+++ sn/inc/nav-trn.php
@@ -44,7 +44,7 @@
       <button class="navbar-toggle" aria-controls="navbar" aria-expanded="true" data-target=".navbar-collapse" data-toggle="collapse" type="button">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-        <span class="icon-bar"></span> 
+        <span class="icon-bar"></span>
       </button>
       <a class="navbar-brand">BookFace</a>
     </div>
@@ -53,32 +53,32 @@
         <li class="dropdown">
           <a class="dropdown-toggle" data-toggle="dropdown" href="#">
           <?php
-          require ("$root/sn/database.php");
+          require("$root/sn/database.php");
           $pdo = Database::connect();
           $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           //update this later when we have info on who logged in user is
           $loggedInUser = "vicky@ucl.ac.uk";
           $sql="SELECT firstName, lastName, profileImage FROM MyDB.users WHERE email='" . $loggedInUser . "'"; //change this to be for logged-in user
-          foreach($pdo->query($sql) as $row){
-            echo "<img style='height:20px;width:20px;border-radius:2px;' src='" . $row["profileImage"] . "'> ";
-            echo $row["firstName"] . " " . $row["lastName"];
+          foreach ($pdo->query($sql) as $row) {
+              echo "<img style='height:20px;width:20px;border-radius:2px;' src='" . $row["profileImage"] . "'> ";
+              echo $row["firstName"] . " " . $row["lastName"];
           }
           Database::disconnect();
           ?>
           <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="/sn/profile/index.php"><i class="fa fa-user"></i> My Profile</a></li>
-            <li><a href="/sn/profile/settings.php"><i class="fa fa-cog"></i> Settings</a></li>
-            <li><a href="/sn/logout.php"><i class="fa fa-sign-out"></i> Logout</a></li>
+            <li><a href="../profile/index.php"><i class="fa fa-user"></i> My Profile</a></li>
+            <li><a href="../profile/settings.php"><i class="fa fa-cog"></i> Settings</a></li>
+            <li><a href="../logout.php"><i class="fa fa-sign-out"></i> Logout</a></li>
           </ul>
         </li>
-        <li><a href="/sn/profile/myfriends.php"><i class="fa fa-users"></i></a></li>
-        <li><a href="/sn/profile/messages.php"><i class="fa fa-comments"></i></a></li>
-        <li><a href="/sn/profile/index.php#4"><i class="fa fa-picture-o"></i></a></li>
-        <li><a href="/sn/photos/index.php">Photos</a></li>
-        <li><a href="/sn/circles/index.php">Circles</a></li>
-        <li><a href="/sn/blog/index.php">Blog</a></li>
-        <li><a href="/sn/explore/index.php">Explore</a></li>
+        <li><a href="../profile/myfriends.php"><i class="fa fa-users"></i></a></li>
+        <li><a href="../profile/messages.php"><i class="fa fa-comments"></i></a></li>
+        <li><a href="../profile/index.php#4"><i class="fa fa-picture-o"></i></a></li>
+        <li><a href="../photos/index.php">Photos</a></li>
+        <li><a href="../circles/index.php">Circles</a></li>
+        <li><a href="../blog/index.php">Blog</a></li>
+        <li><a href="../explore/index.php">Explore</a></li>
         <?php
         //Check if logged in user is administrator
         $pdo = Database::connect();
@@ -87,8 +87,8 @@
         //echo $sql;
         $res=$pdo->query($sql);
         $row = $res->fetch(PDO::FETCH_ASSOC);
-        if($row["roleTitle"]=="administrator"){
-          echo "<li><a href=\"/sn/admin/index.php\">Admin</a></li>";
+        if ($row["roleTitle"]=="administrator") {
+            echo "<li><a href=\"../admin/index.php\">Admin</a></li>";
         }
         Database::disconnect();
         ?>
@@ -102,4 +102,3 @@
       </div>
     </div>
 </nav>
-

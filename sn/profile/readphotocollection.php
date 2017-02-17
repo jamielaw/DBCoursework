5b98ac3eb638bd76571def262584f09648673d96
Diana Darie <zcabdad@ucl.ac.uk>
2017-02-17 21:20:20 +0000

    ui changes

--- sn/profile/readphotocollection.php
+++ sn/profile/readphotocollection.php
@@ -1,35 +1,35 @@
-<?php 
-	$title = "Bookface Social Network";
-	$description = "A far superior social network";
-	include("../inc/header.php"); 
-	include("../inc/nav-trn.php"); 
-	//require '../database.php';
-	
-	$createdBy = null;
-	$photoCollectionId = null;
-	if ( !empty($_GET['createdBy'])) {
-		$createdBy = $_REQUEST['createdBy'];
-	}
-	if ( !empty($_GET['photoCollectionId'])) {
-		$photoCollectionId = $_REQUEST['photoCollectionId'];
-	}
-	
-	
-	if ( null==$createdBy || null==$photoCollectionId) {
-		header("Location: index.php");
-	} else {
-		$pdo = Database::connect();
-		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "SELECT * FROM photos JOIN photocollection ON photocollection.photoCollectionId = photos.photoCollectionId WHERE photocollection.createdBy = ? AND photocollection.photoCollectionId = ?";
-		$q = $pdo->prepare($sql);
-		$q->execute(array($createdBy, $photoCollectionId));
-		//$data = $q->fetch(PDO::FETCH_ASSOC);
+<?php
+    $title = "Bookface Social Network";
+    $description = "A far superior social network";
+    include("../inc/header.php");
+    include("../inc/nav-trn.php");
+    //require '../database.php';
 
-		$sql2 = "SELECT * FROM photocollection WHERE photoCollectionId = ?";
-		$q2 = $pdo->prepare($sql2);
-		$q2->execute(array($photoCollectionId));
-		Database::disconnect();
-	}
+    $createdBy = null;
+    $photoCollectionId = null;
+    if (!empty($_GET['createdBy'])) {
+        $createdBy = $_REQUEST['createdBy'];
+    }
+    if (!empty($_GET['photoCollectionId'])) {
+        $photoCollectionId = $_REQUEST['photoCollectionId'];
+    }
+
+
+    if (null==$createdBy || null==$photoCollectionId) {
+        header("Location: index.php");
+    } else {
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT * FROM photos JOIN photocollection ON photocollection.photoCollectionId = photos.photoCollectionId WHERE photocollection.createdBy = ? AND photocollection.photoCollectionId = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($createdBy, $photoCollectionId));
+        //$data = $q->fetch(PDO::FETCH_ASSOC);
+
+        $sql2 = "SELECT * FROM photocollection WHERE photoCollectionId = ?";
+        $q2 = $pdo->prepare($sql2);
+        $q2->execute(array($photoCollectionId));
+        Database::disconnect();
+    }
 ?>
 
 <!DOCTYPE html>
@@ -44,49 +44,50 @@
 </head>
 
 <body>
-	<div class=" jumbotron">
+	<div class="jumbotron">
     	<div class="row">
-    		<?php 
-    			$row = $q2->fetch(PDO::FETCH_ASSOC)
-    		?>
-    		<div style="float: left;" class="form-actions padding">
-				<a class="btn btn-info" href="index.php">Back</a>
-			</div>  
-	    	<h3 class="text-center"><?php echo $row['title'];?></h3>
-	    	<h5 class="text-center"><?php echo $row['description'];?></h5>
+    		<?php
+                $row = $q2->fetch(PDO::FETCH_ASSOC)
+                ?>
+	    	<h3 class="col-md-2 col-md-offset-5"><?php echo $row['title'];?></h3>
 	    </div>
-	</div>
-    
-    <div>
-	<form class="padding" action="uploadphoto.php?photoCollectionId=<?php echo $photoCollectionId; ?>" method="post" enctype="multipart/form-data">
-		Select image to upload:
-		<input type="file" name="fileToUpload" id="fileToUpload"> <br>
-		<input class="btn btn-primary" type="submit" value="Upload Image" name="submit">
-	</form>  
-	</div>
-	<br>
+		</div>
 
+ <div>
+	<form class="col-md-2 col-md-offset-5" action="uploadphoto.php?photoCollectionId=<?php echo $photoCollectionId; ?>" method="post" enctype="multipart/form-data">
+		<p class=""> Select image to upload: </p>
+		<input class="" type="file" name="fileToUpload" id="fileToUpload"> <br>
+		<input class= "btn btn-primary" type="submit" value="Upload Image" name="submit">
+	</form>
+	</div>
+
+ <div class="row"><br></div>
+ <div class="row"><br></div>
+
+	<div class="row">
     <div class="container">
 	    	<div class="form-horizontal" >
-			  <div class="control-group">	
+			  <div class="control-group">
 				  	<div class="controls"></div>
 					<div class="controls">
 						   <label class="checkbox">
-						    	<?php 
-						   	 	while ($row = $q->fetch(PDO::FETCH_ASSOC)){
-						     	?>
-						     	<a href="readphoto.php?photoId=<?php echo $row['photoId']?>&imageReference=<?php echo $row['imageReference']?>&photoCollectionId=<?php echo $row['photoCollectionId'];?>" id="bottle" >
-									<img src="<?php echo $row['imageReference'];?>"  height="200" ></td></a>
+						    	<?php
+                                    while ($row = $q->fetch(PDO::FETCH_ASSOC)) {
+                                        ?>
+						     	<a href="readphoto.php?photoId=<?php echo $row['photoId']?>&imageReference=<?php echo $row['imageReference']?>&photoCollectionId=<?php echo $row['photoCollectionId']; ?>" id="bottle" >
+									<img src="<?php echo $row['imageReference']; ?>"  height="200" ></td></a>
 						     	<?php
-						     	 }
-						     	?>
+
+                                    }
+                                ?>
 						    </label>
 					</div>
 					</div>
 			  </div>
 		   </div>
 		</div>
-				
+	</div>
+
     </div> <!-- /container -->
   </body>
     <?php include '../inc/footer.php'; ?>

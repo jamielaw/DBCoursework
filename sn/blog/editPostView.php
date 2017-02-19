cb232f0c9d45f7cc2558ceed55261b034b93898f
vickyd885 <vickyd885@gmail.com>
2017-02-19 18:12:34 +0000

    UI changes

--- sn/blog/editPostView.php
+++ sn/blog/editPostView.php
@@ -25,33 +25,34 @@
 
 <body>
   <div class="container">
-    <div class="span10 offset1">
+    <div class="">
       <div class="row">
-        <font size="5">Update Blog Post</font>
-      </div>
-      <button id="previewBtn" > Preview </button>
-      <button id="editBtn" style=""> Edit </button>
+        <h1 style="margin-bottom: 15px;"> Update Blog Post</h1>
+
+      <button class="btn btn-info" id="previewBtn" > Preview </button>
+      <button class="btn btn-warning" id="editBtn" style=""> Edit </button>
 
       <form class="form-horizontal" method="POST" action="editPost.php">
         <input type="hidden" name="argument1" value="<?php echo $argument1;?>">
-        <div class="control-group">
-          <label class="control-label">Blog Title:</label>
-          <div class="controls">
-            <input type="text" name="blogTitle" value="<?php echo $row['blogTitle'];?>"> <br>
+        <div id="blogTitleBar">
+          <div class="control-group">
+            <label class="control-label">Blog Title:</label>
+            <div class="controls">
+              <input type="text" name="blogTitle" style="width: 30vw;"value="<?php echo $row['blogTitle'];?>"> <br>
+            </div>
           </div>
         </div>
         <div class="control-group">
-
           <div class="controls" id="editArea">
           <label class="control-label">Content:</label>
-            <textarea id="rawText" style="width:100%" name="blogDescription" ><?php echo $row['blogDescription'];?></textarea> <br>
+            <textarea id="rawText" style="width:100%; height: 55vh;" name="blogDescription" ><?php echo $row['blogDescription'];?></textarea> <br>
           </div>
-          <div id="MDpreview" style="width:100%;">
-
+          <div id="MDpreview" style="width:100%; text-align:center;">
           </div>
         </div>
-        <button type="submit">Update!</button>
+        <button class="btn btn-success" type="submit">Save</button>
       </form>
+      </div>
     </div>
     </div>
 
@@ -65,21 +66,20 @@ $(document).ready(function(){
     var converter = new showdown.Converter(),
         text      = $('#rawText').val();
         html      =  converter.makeHtml(text);
-    $("#MDpreview").html(html);// = html;
+    $("#MDpreview").html(html);
     $("#MDpreview").show();
     $("#editArea").hide();
     $("#editBtn").show();
+    $("#blogTitleBar").hide();
 
   });
 
   $("#editBtn").click(function(){
     $("#MDpreview").hide();
     $("#editArea").show();
+    $("#blogTitleBar").show();
   });
 
 });
 
 </script>
-
-
-    <?php include '../inc/footer.php'; ?>

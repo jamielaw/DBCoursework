965d617acb5da4073e067450cbe3af5b98e83ba7
Diana Darie <zcabdad@ucl.ac.uk>
2017-02-19 11:20:41 +0000

    access rights setting up (UI+fetching friends/circles/fof)

--- sn/profile/readprofile.php
+++ sn/profile/readprofile.php
@@ -30,9 +30,17 @@
         $sql = 'SELECT DISTINCT email, firstName, lastName, profileImage FROM users JOIN friendships ON users.email = friendships.emailFrom OR users.email=friendships.emailTo WHERE (friendships.emailFrom=? OR friendships.emailTo=?) AND users.email!=? AND status=\'accepted\';';
         $q = $pdo->prepare($sql);
         $q->execute(array($email,$email,$email));
-        $data = $q->fetch(PDO::FETCH_ASSOC);
-          //echo $data;
-          return $data;
+        return $q;
+    }
+
+    function getCircles($email)
+    {
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = 'SELECT c.circleOfFriendsName, c.circleFriendsId FROM circleoffriends c INNER JOIN usercirclerelationships u ON c.circleFriendsId = u.circleFriendsId WHERE u.email=?';
+        $q = $pdo->prepare($sql);
+        $q->execute(array($email));
+        return $q;
     }
 
     function getPhoto($photoId)
@@ -43,8 +51,24 @@
         $q = $pdo->prepare($sql);
         $q->execute(array($photoId));
         $data = $q->fetch(PDO::FETCH_ASSOC);
-          //echo $data;
-          return $data;
+        return $data;
+    }   
+
+    function getFriendsOfFriends($email)
+    {
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = 'SELECT DISTINCT email, firstName, lastName, profileImage FROM users JOIN friendships ON users.email = friendships.emailFrom OR users.email=friendships.emailTo WHERE 
+        (friendships.emailFrom IN
+            (SELECT DISTINCT email FROM users JOIN friendships ON users.email = friendships.emailFrom OR users.email=friendships.emailTo WHERE (friendships.emailFrom=? OR friendships.emailTo=?) AND users.email!=? AND status=\'accepted\') 
+        OR friendships.emailTo IN
+            (SELECT DISTINCT email FROM users JOIN friendships ON users.email = friendships.emailFrom OR users.email=friendships.emailTo WHERE (friendships.emailFrom=? OR friendships.emailTo=?) AND users.email!=? AND status=\'accepted\')) 
+        AND users.email NOT IN 
+            (SELECT DISTINCT email FROM users JOIN friendships ON users.email = friendships.emailFrom OR users.email=friendships.emailTo WHERE (friendships.emailFrom=? OR friendships.emailTo=?) AND users.email!=? AND status=\'accepted\')
+        AND users.email!=?';
+        $q = $pdo->prepare($sql);
+        $q->execute(array($email,$email,$email,$email,$email,$email,$email,$email,$email,$email));
+        return $q;
     }
 
     function checkDescription($description)
@@ -238,6 +262,10 @@
 										</a>
 
 										<div class="tools tools-bottom">
+                                            <a data-title="'.$row['title'].'" data-description="'.$row['description'].'" data-id="'.$row['photoCollectionId'].'" class="open-update_dialog2" data-toggle="modal" href="#update_dialog2">
+                                                <i class="ace-icon fa fa-eye"></i>
+                                            </a>
+
 											<a data-title="'.$row['title'].'" data-description="'.$row['description'].'" data-id="'.$row['photoCollectionId'].'" class="open-update_dialog" data-toggle="modal" href="#update_dialog">
 												<i class="ace-icon fa fa-pencil"></i>
 											</a>
@@ -299,6 +327,79 @@
             </div>
         </div>
 
+        <!-- modal to update access rights -->
+        <!-- the div that represents the modal dialog -->
+        <div class="modal fade" id="update_dialog2" role="dialog">
+            <div class="modal-dialog">
+               <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Update Access Rights</h4>
+                    </div>
+                    <div class="modal-body">
+                        <form id="update_form" action="updatephotocollection.php" method="POST">
+                            <div class="tabbable">
+                                <ul class="nav nav-tabs padding-18">
+                                    <li class="active">
+                                        <a data-toggle="tab" href="#friends2">
+                                            <i class="green ace-icon fa fa-user-plus bigger-120"></i>
+                                            Friends
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a data-toggle="tab" href="#circles">
+                                            <i class="blue ace-icon fa fa-users bigger-120"></i>
+                                            Circles
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a data-toggle="tab" href="#friendsoffriends">
+                                            <i class="pink ace-icon fa fa-user bigger-120"></i>
+                                            Friends of Friends
+                                        </a>
+                                    </li>
+                                </ul>
+                                <div class="tab-content no-border padding-24">
+                                    <div id="friends2" class="tab-pane in active">
+                                        <ul id="check-list-box" class="list-group checked-list-box">
+                                         <?php
+                                            foreach (getFriends($email) as $row) {
+                                                echo '<li class="list-group-item">'.$row['firstName'].' '.$row['lastName'].'</li>';
+                                            }
+                                        ?>
+                                        </ul>  
+                                    </div>
+                                    <div id="circles" class="tab-pane">
+                                        <ul id="check-list-box" class="list-group checked-list-box">
+                                         <?php
+                                            foreach (getCircles($email) as $row) {
+                                                echo '<li class="list-group-item">'.$row['circleOfFriendsName'].'</li>';
+                                            }
+                                        ?>
+                                        </ul>  
+                                    </div>
+                                    <div id="friendsoffriends" class="tab-pane">
+                                        <ul id="check-list-box" class="list-group checked-list-box">
+                                         <?php
+                                            foreach (getFriendsOfFriends($email) as $row) {
+                                                echo '<li class="list-group-item">'.$row['firstName'].' '.$row['lastName'].'</li>';
+                                            }
+                                        ?>
+                                        </ul>                                     
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" id="submitForm2" class="btn btn-success">Update</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <!-- modal to update collection -->
         <!-- the div that represents the modal dialog -->
         <div class="modal fade" id="update_dialog" role="dialog">
@@ -353,6 +454,87 @@
 </html>
 
 <script>
+/* access right modal*/
+
+$(function () {
+    $('.list-group.checked-list-box .list-group-item').each(function () {
+        
+        // Settings
+        var $widget = $(this),
+            $checkbox = $('<input type="checkbox" class="hidden" />'),
+            color = ($widget.data('color') ? $widget.data('color') : "primary"),
+            style = ($widget.data('style') == "button" ? "btn-" : "list-group-item-"),
+            settings = {
+                on: {
+                    icon: 'glyphicon glyphicon-check'
+                },
+                off: {
+                    icon: 'glyphicon glyphicon-unchecked'
+                }
+            };
+            
+        $widget.css('cursor', 'pointer')
+        $widget.append($checkbox);
+
+        // Event Handlers
+        $widget.on('click', function () {
+            $checkbox.prop('checked', !$checkbox.is(':checked'));
+            $checkbox.triggerHandler('change');
+            updateDisplay();
+        });
+        $checkbox.on('change', function () {
+            updateDisplay();
+        });
+          
+
+        // Actions
+        function updateDisplay() {
+            var isChecked = $checkbox.is(':checked');
+
+            // Set the button's state
+            $widget.data('state', (isChecked) ? "on" : "off");
+
+            // Set the button's icon
+            $widget.find('.state-icon')
+                .removeClass()
+                .addClass('state-icon ' + settings[$widget.data('state')].icon);
+
+            // Update the button's color
+            if (isChecked) {
+                $widget.addClass(style + color + ' active');
+            } else {
+                $widget.removeClass(style + color + ' active');
+            }
+        }
+
+        // Initialization
+        function init() {
+            
+            if ($widget.data('checked') == true) {
+                $checkbox.prop('checked', !$checkbox.is(':checked'));
+            }
+            
+            updateDisplay();
+
+            // Inject the icon if applicable
+            if ($widget.find('.state-icon').length == 0) {
+                $widget.prepend('<span class="state-icon ' + settings[$widget.data('state')].icon + '"></span>');
+            }
+        }
+        init();
+    });
+    
+    $('#get-checked-data').on('click', function(event) {
+        event.preventDefault(); 
+        var checkedItems = {}, counter = 0;
+        $("#check-list-box li.active").each(function(idx, li) {
+            checkedItems[counter] = $(li).text();
+            counter++;
+        });
+        $('#display-json').html(JSON.stringify(checkedItems, null, '\t'));
+    });
+});
+
 /* must apply only after HTML has loaded */
 $(document).ready(function () {
 

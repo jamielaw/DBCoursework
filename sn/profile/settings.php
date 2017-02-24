631d0dac5106b0e86bbfd3655ae1ea6d1797ec33
JAMIE LAW <jamie.law.14@ucl.ac.uk>
2017-02-24 19:31:43 +0000

    update settings page

--- sn/profile/settings.php
+++ sn/profile/settings.php
@@ -10,20 +10,21 @@
 <body>
 	<div class="container">
 		<h1>Your Privacy Settings</h1>
-		<?php //we want to get the privacy settings of the user here and echo it accordingly
-			$sql = "SELECT * FROM MyDB.privacySettings WHERE email='" . $loggedInUser . "'";
-			foreach($pdo->query($sql) as $row){
-				if($row["status"]==True){
-					echo "<input type='checkbox' name='" . $row["privacySettingsId"] . "' value = '" . $row["privacySettingsTitle"] . "' checked>" . $row["privacySettingsDescription"] . "</input>";
+		<form class="" action="createcircle.php" method="get" id="createform">
+			<?php //we want to get the privacy settings of the user here and echo it accordingly
+				$sql = "SELECT * FROM MyDB.privacySettings WHERE email='" . $loggedInUser . "'";
+				foreach($pdo->query($sql) as $row){
+					echo "<h3>" . $row["privacySettingsTitle"] . "</h3>";
+					if($row["status"]==True){
+						echo "<input type='checkbox' name='" . $row["privacySettingsId"] . "' value = '" . $row["privacySettingsTitle"] . "' checked>" . $row["privacySettingsDescription"] . "</input>";
+					}
+					else{
+						echo "<input type='checkbox' name='" . $row["privacySettingsId"] . "' value = '" . $row["privacySettingsTitle"] . "'>" . $row["privacySettingsDescription"] . "</input>";
+					}
+					echo "<br>";
 				}
-				else{
-					echo "<input type='checkbox' name='" . $row["privacySettingsId"] . "' value = '" . $row["privacySettingsTitle"] . "'>" . $row["privacySettingsDescription"] . "</input>";
-				}
-				echo "<br>";
-			}
-		?>
-		<form type="submit">
-			<button>Update Settings</button>
+			?>
+			<button type="submit">Update Settings</button>
 		</form>
 	</div>
 </body>

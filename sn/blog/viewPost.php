c9788ce6d902784b1c8f01c45a7f93732d125227
vickyd885 <vickyd885@gmail.com>
2017-02-24 13:02:42 +0000

    fix the dumbest tab bug in the world

--- sn/blog/viewPost.php
+++ sn/blog/viewPost.php
@@ -7,44 +7,35 @@
     $title = "Blog";
     $description = "";
 
-
-    // CHANGED THIS TO BE AUTHENTICATED LATER
-    $loggedInUser = "larry@ucl.ac.uk";
     $pdo = Database::connect();
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $postQuery = "SELECT * FROM blogs WHERE blogId=" . $_GET["blogId"];
+    $postQuery = "SELECT * FROM blogs WHERE blogId=" . htmlspecialchars($_GET['blogId']);
 
     $y = $pdo->prepare($postQuery);
     $y->execute();
     $postQueryResult = $y->fetch(PDO::FETCH_ASSOC);
-
-
 
   ?>
   <div class="container">
     <div class="blog-container">
       <div class="row">
         <font size="10"> <?php echo $postQueryResult["blogTitle"]; ?> </font> <br>
-        <font size="3"></font>
-      </div>
+
       <div class="blog-edit-options">
         <a class='btn btn-success' href="editPostView.php?blogId=<?php echo  $postQueryResult["blogId"]; ?>"> <i class="fa fa-pencil" aria-hidden='true'> Edit</i>  </a>
         <a class='btn btn-danger' href="deleteBlog.php?blogId=<?php echo  $postQueryResult["blogId"]; ?>"> <i class="fa fa-trash" aria-hidden='true'> Delete</i>  </a>
-
       </div>
 
-    <div id="rawText">
-        <?php echo $postQueryResult["blogDescription"];  ?>
+    <div id="rawText" style="font-size: 14px; visibility:hidden;"><?php echo $postQueryResult["blogDescription"];  ?></div>
+    <div id="viewText">
+
     </div>
 
-
+          </div>
 
 
       </div>
-
-
-
 
 
 
@@ -61,13 +52,12 @@
 <script>
 
 $(document).ready(function(){
-  var converter = new showdown.Converter(),
-      text      = $('#rawText')[0]['textContent'];
-      html      =  converter.makeHtml(text);
+  var converter = new showdown.Converter();
+  var text      = $('#rawText')[0]['innerHTML'];
+  var html      =  converter.makeHtml(text);
 
   console.log(text);
-  console.log(html);
-  $("#rawText").html(html);// = html;
+  $("#viewText").html(html);
 });
 
 

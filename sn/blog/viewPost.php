e9cbcfcb862a45a987839e146502f663f95b9d20
vickyd885 <vickyd885@gmail.com>
2017-03-01 09:26:23 +0000

    add logic to view post so it doesnt render edit options when visiting someone elses blog

--- sn/blog/viewPost.php
+++ sn/blog/viewPost.php
@@ -16,16 +16,19 @@
     $y->execute();
     $postQueryResult = $y->fetch(PDO::FETCH_ASSOC);
 
+
   ?>
   <div class="container">
     <div class="blog-container">
       <div class="row">
         <font size="10"> <?php echo $postQueryResult["blogTitle"]; ?> </font> <br>
 
+      <?php if( $loggedInUser == $postQueryResult["email"]){ ?>
       <div class="blog-edit-options">
         <a class='btn btn-success' href="editPostView.php?blogId=<?php echo  $postQueryResult["blogId"]; ?>"> <i class="fa fa-pencil" aria-hidden='true'> Edit</i>  </a>
         <a class='btn btn-danger' href="deleteBlog.php?blogId=<?php echo  $postQueryResult["blogId"]; ?>"> <i class="fa fa-trash" aria-hidden='true'> Delete</i>  </a>
       </div>
+    <?php } ?>
 
     <div id="rawText" style="font-size: 14px; visibility:hidden;"><?php echo $postQueryResult["blogDescription"];  ?></div>
     <div id="viewText">

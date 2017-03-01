e46cd05026d55317e905497059ec235edd9ae627
vickyd885 <vickyd885@gmail.com>
2017-03-01 13:58:57 +0000

    add edits for blogs

---
+++ sn/admin/blogs/editPost.php
@@ -0,0 +1,43 @@
+<?php
+  // DB Auth Script
+  require '../../database.php';
+
+  // Given "string", returns "'string'" - useful for SQL queries
+  function wrapArgument($arg){
+    $arg = htmlspecialchars($arg);
+    return "'" . $arg . "'";
+  }
+  // Removes spaces
+  function removeSpaces($arg){
+    return str_replace(' ', '', $arg);
+  }
+  // All functions should be moved to utils.php at some point
+  function redirect($url) {
+    ob_start();
+    header('Location: '.$url);
+    ob_end_flush();
+    die();
+  }
+
+  $pdo = Database::connect();
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $sql = "UPDATE blogs SET "
+  . "blogTitle=" . wrapArgument($_POST['blogTitle']) .  ","
+  . "blogDescription=" . wrapArgument($_POST['blogDescription'])
+  . " WHERE blogId=" . htmlspecialchars($_POST['argument1']);
+
+  #echo $sql;
+  $pdo->exec($sql);
+
+  // Need to handle error catching etc
+  Database::disconnect();
+
+  // Direct back to sn/admin
+  redirect('../../admin');
+
+
+
+
+
+?>

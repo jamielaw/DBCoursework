b8a051a87275fe83d4713f4541213cf4109ce296
moultz <henry@moultz.com>
2017-03-02 14:31:10 +0000

    Added session_start to index.php

--- sn/index.php
+++ sn/index.php
@@ -3,27 +3,17 @@ $title = "Bookface Social Network";
 $description = "A far superior social network";
 include("inc/header.php");
 include ("inc/nav-trn.php");
-
+session_start();
  ?>
   <body>
     <!--  No Navigation because this is a page for those who aren't authenticated -->
-      <!--  LOGIN -->
-      <form class="" action="login.php" method="post">
-        <input type="text" name="uid" placeholder="username">
-        <br>
-        <input type="password" name="pwd" placeholder="password">
-        <br>
-        <button type="submit">LOGIN</button>
-      </form>
-
       <?php
-        if (isset($_SESSION['id'])) {
-          echo $_SESSION['id'];
+        if (isset($_SESSION['loggedInUserEmail'])) {
+          echo $_SESSION['loggedInUserEmail'];
         } else {
           echo "You are not logged in";
         }
        ?>
-
       <br/>
       <br/>
       <br/>

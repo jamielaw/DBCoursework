abe8316b0df629609a2a28ad53568e883e021832
moultz <henry@moultz.com>
2017-03-02 14:47:32 +0000

    got login&sign up or logout showing conditional to the SESSION variable being set

--- sn/index.php
+++ sn/index.php
@@ -7,38 +7,53 @@ session_start();
  ?>
   <body>
     <!--  No Navigation because this is a page for those who aren't authenticated -->
+      <!--  LOGIN -->
       <?php
         if (isset($_SESSION['loggedInUserEmail'])) {
-          echo $_SESSION['loggedInUserEmail'];
+          echo "You are logged in";
+          echo "
+                <form action='logout.php' method='post'>
+                  <button type='submit'>LOG OUT</button>
+                </form>
+                ";
+          $_SESSION['loggedInUserEmail'];
         } else {
-          echo "You are not logged in";
+          echo "You are not logged in, log in or sign up";
+          echo "<form class='' action='login.php' method='post'>
+            <input type='text' name='email' placeholder='email'>
+            <br>
+            <input type='password' name='pwd' placeholder='password'>
+            <br>
+            <button type='submit'>LOGIN</button>
+          </form>";
+          echo "
+          <!--  SIGNUP -->
+          <form action='signup.php' method='post'>
+            <br>
+            <input type='text' name='first' placeholder='first'/>
+            <br>
+            <input type='text' name='last' placeholder='last'/>
+            <br>
+            <input type='text' name='email' placeholder='email' />
+            <br>
+            <input type='password' name='pwd' placeholder='password'/>
+            <br>
+            <button type='submit'>SIGN UP</button>
+          </form>
+          ";
         }
        ?>
-      <br/>
-      <br/>
-      <br/>
-
-      <!--  SIGNUP -->
-      <form action="signup.php" method="post">
-        <br>
-        <input type="text" name="first" placeholder="first"/>
-        <br>
-        <input type="text" name="last" placeholder="last"/>
-        <br>
-        <input type="text" name="email" placeholder="email" />
-        <br>
-        <input type="password" name="pwd" placeholder="password"/>
-        <br>
-        <button type="submit">SIGN UP</button>
-      </form>
 
       <br/>
       <br/>
       <br/>
 
-      <form action="logout.php" method="post">
-        <button type="submit">LOG OUT</button>
-      </form>
+
+
+      <br/>
+      <br/>
+      <br/>
+
     </body>
 
     <!-- Footer  -->

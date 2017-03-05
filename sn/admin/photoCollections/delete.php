6f0750b45f0e3a45b939eaa278229acbd4b34c19
vickyd885 <vickyd885@gmail.com>
2017-03-05 16:59:18 +0000

    PC stuff

--- sn/admin/photoCollections/delete.php
+++ sn/admin/photoCollections/delete.php
@@ -13,10 +13,27 @@
   // Get PK of Table
 
   // sql to delete a record
-  $sql = "DELETE FROM photoCollection WHERE photoCollectionId=".   $_GET['pcID'];
-  //echo $sql;
   $pdo = Database::connect();
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $foreignKey = $_GET['pcId'];
+
+  $pcData = "SELECT * FROM photos WHERE photoCollectionId=$foreignKey";
+
+  foreach($pdo->query($pcData) as $row){
+
+    $comments = "DELETE FROM comments WHERE photoId=".  $row['photoId'];
+    $annotations = "DELETE FROM annotations WHERE photoId=". $row['photoId'];
+    $sql = "DELETE FROM photos WHERE photoId=".  $row['photoId'];
+
+    $pdo->exec($comments);
+    $pdo->exec($annotations);
+    $pdo->exec($sql);
+  }
+
+
+  $sql = "DELETE FROM photoCollection WHERE photoCollectionId=".   $_GET['pcId'];
+  //echo $sql;
+
   $pdo->exec($sql);
   Database::disconnect();
 

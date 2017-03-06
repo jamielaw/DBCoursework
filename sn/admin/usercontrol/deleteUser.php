4c79777021088ae03d5bb3f106f6cc7ee31623cb
vickyd885 <vickyd885@gmail.com>
2017-03-06 22:25:55 +0000

    add queries to delete a user woot woot

--- sn/admin/usercontrol/deleteUser.php
+++ sn/admin/usercontrol/deleteUser.php
@@ -9,14 +9,58 @@
     ob_end_flush();
     die();
   }
+  $pdo = Database::connect();
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   // Get PK of Table
   $argument1 = htmlspecialchars( $_GET['email']);
   // sql to delete a record
+  // delete all the friends hes been involved in
+  $deleteFriends = "DELETE FROM friendships WHERE emailTo = '$argument1' OR emailFrom = '$argument1'";
+  $pdo->exec($deleteFriends);
+
+
+  $deleteMessages = "DELETE FROM messages WHERE emailTo = '$argument1' OR emailFrom = '$argument1'";
+  $pdo->exec($deleteMessages);
+
+  //delete privact settings
+  $deletePrivacy = "DELETE FROM privacySettings WHERE email= '$argument1'";
+  $pdo->exec($deletePrivacy);
+
+  //delete privact settings
+  $deleteBlogs = "DELETE FROM blogs WHERE email= '$argument1'";
+  $pdo->exec($deleteBlogs);
+
+  //delete comments
+  $deleteComments= "DELETE FROM comments WHERE email= '$argument1'";
+  $pdo->exec($deleteComments);
+
+  //delete annotations
+  $deleteAnno= "DELETE FROM annotations WHERE email= '$argument1'";
+  $pdo->exec($deleteAnno);
+
+
+  $pcData = "SELECT * FROM photoCollection WHERE createdBy='$argument1'";
+  foreach($pdo->query($pcData) as $row){
+
+    $sql = "DELETE FROM photos WHERE photoCollectionId=".  $row['photoCollectionId'] ;
+
+    $pdo->exec($sql);
+  }
+  $deletePC= "DELETE FROM photoCollection WHERE createdBy= '$argument1'";
+  $pdo->exec($deletePC);
+
+  //delete access rights
+  $deleteAccessRights= "DELETE FROM accessRights WHERE email= '$argument1'";
+  $pdo->exec($deleteAccessRights);
+
+
+  $deleteCircleFriends = "DELETE FROM userCircleRelationships WHERE email= '$argument1'";
+  $pdo->exec($deleteCircleFriends);
   $sql = "DELETE FROM users WHERE email=". "'" . $argument1 . "'";
-  $pdo = Database::connect();
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $pdo->exec($sql);
+
+
   Database::disconnect();
 
   //Redirect to /sn/admin page to create "refresh "
